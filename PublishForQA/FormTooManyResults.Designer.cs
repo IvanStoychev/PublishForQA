c2d50f655196e6024ceb890d11eb4687e2756c75
Ivan Stoychev <ivanstoychev44@gmail.com>
2017-10-19 16:25:26 +0300

    Get the longest result and size ListBoxes appropriately.

--- PublishForQA/FormTooManyResults.Designer.cs
+++ PublishForQA/FormTooManyResults.Designer.cs
@@ -28,21 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tblpPrimary = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
-            // 
-            // tblpPrimary
-            // 
-            this.tblpPrimary.AutoSize = true;
-            this.tblpPrimary.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
-            this.tblpPrimary.ColumnCount = 1;
-            this.tblpPrimary.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblpPrimary.Location = new System.Drawing.Point(12, 12);
-            this.tblpPrimary.Name = "tblpPrimary";
-            this.tblpPrimary.RowCount = 1;
-            this.tblpPrimary.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tblpPrimary.Size = new System.Drawing.Size(260, 238);
-            this.tblpPrimary.TabIndex = 0;
             // 
             // FormTooManyResults
             // 
@@ -50,20 +36,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(984, 966);
-            this.Controls.Add(this.tblpPrimary);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.ClientSize = new System.Drawing.Size(441, 428);
             this.MinimizeBox = false;
             this.Name = "FormTooManyResults";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Choose paths";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tblpPrimary;
     }
 }
--- PublishForQA/FormTooManyResults.cs
+++ PublishForQA/FormTooManyResults.cs
@@ -15,7 +15,7 @@ namespace PublishForQA
         public FormTooManyResults()
         {
             List<string> eCheckPath = new List<string>() { "qwhfoqwjidwhpaiwjdpwaijdpawijsdapisdjpidpIQJDPAWIJFPAIFPAIJDPAWIDJAPWIFJAPFDIJPAFHAPIFPAFJAEPFOEFJO1", "l"};
-            List<string> eCheckCorePath = new List<string>() { "qwhfoqwjidwhpaiwjdpwaijdpawijsdapisdjpidpIQJDPAWIJFPAIFPAIJDPAWIDJAPWIFJAPFDIJPAFHAPIFPAFJAEPFOEFJO1adawdawdwad", "o" };
+            List<string> eCheckCorePath = new List<string>() { "qwhfoqwjidwhpaiwjdFJO1adawdawdwad", "o" };
             //params - List<string> eCheckPath, List<string> eCheckCorePath
             InitializeComponent();
 
@@ -36,6 +36,28 @@ namespace PublishForQA
             tlpMain.Location = new Point(12, 12);
             this.Controls.Add(tlpMain);
 
+            //We create a list that is going to hold all the results
+            List<string> longestList = new List<string>();
+
+            //If there are more than 1 result for both ECheck AND ECheckCore
+            //"longestList" becomes the sum of all their results.
+            if (eCheckPath.Count > 1 && eCheckCorePath.Count > 1)
+            {
+                longestList = eCheckPath.Concat(eCheckCorePath).ToList();
+            }
+            //If there is more than 1 result for Echeck, only
+            else if (eCheckPath.Count > 1 && eCheckCorePath.Count < 2)
+            {
+                longestList = eCheckPath;
+            }
+            //If there is more than 1 result for EcheckCore, only
+            else if (eCheckPath.Count < 2 && eCheckCorePath.Count > 1)
+            {
+                longestList = eCheckCorePath;
+            }
+
+            string longest = longestList.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur);
+
             if (eCheckPath.Count > 1)
             {
                 Panel pEcheck = new Panel();
@@ -49,16 +71,14 @@ namespace PublishForQA
                 lblECheck.UseMnemonic = false;
 
                 ListBox lbECheck = new ListBox();
-                List<string> longestList = eCheckPath.Concat(eCheckCorePath).ToList();
-                string longest = longestList.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur);
+                
                 Graphics graphics = this.CreateGraphics();
-                SizeF size = graphics.MeasureString(longest, lblECheck.Font);
-                size.Width = (float)(size.Width * 0.94);
+                SizeF size = graphics.MeasureString(longest, lbECheck.Font);
+                size.Width = MeasureString(longest, lbECheck.Font);
                 size.Height = 15 * eCheckPath.Count;
                 lbECheck.Size = Size.Round(size);
                 lbECheck.DataSource = eCheckPath;
                 lbECheck.Location = new Point(0, 15);
-                //lbECheck.ScrollAlwaysVisible = true;
 
                 tlpMain.RowCount++;
                 tlpMain.RowStyles.Add(new RowStyle(SizeType.Absolute, 100));
@@ -69,9 +89,80 @@ namespace PublishForQA
             if (eCheckCorePath.Count > 1)
             {
                 Panel pCore = new Panel();
+                pCore.Dock = DockStyle.Fill;
+                pCore.AutoSize = true;
+                pCore.AutoScroll = true;
+
+                Label lblECheck = new Label();
+                lblECheck.Text = "E-Check location:";
+                lblECheck.AutoSize = true;
+                lblECheck.UseMnemonic = false;
+
+                ListBox lbECheck = new ListBox();
+
+                Graphics graphics = this.CreateGraphics();
+                SizeF size = graphics.MeasureString(longest, lbECheck.Font);
+                size.Width = MeasureString(longest, lbECheck.Font);
+                size.Height = 15 * eCheckPath.Count;
+                lbECheck.Size = Size.Round(size);
+                lbECheck.DataSource = eCheckPath;
+                lbECheck.Location = new Point(0, 15);
+
+                tlpMain.RowCount++;
+                tlpMain.RowStyles.Add(new RowStyle(SizeType.Absolute, 100));
+                tlpMain.Controls.Add(pCore, 0, tlpMain.RowCount - 1);
+                pCore.Controls.Add(lblECheck);
+                pCore.Controls.Add(lbECheck);
+            }
+        }
+
+        static public int MeasureDisplayStringWidth2(Graphics graphics, string text, Font font)
+        {
+            System.Drawing.StringFormat format = new System.Drawing.StringFormat();
+            System.Drawing.RectangleF rect = new System.Drawing.RectangleF(0, 0, 1000, 1000);
+            System.Drawing.CharacterRange[] ranges = {new System.Drawing.CharacterRange(0, text.Length) };
+            System.Drawing.Region[] regions = new System.Drawing.Region[1];
+
+            format.SetMeasurableCharacterRanges(ranges);
+
+            regions = graphics.MeasureCharacterRanges(text, font, rect, format);
+            rect = regions[0].GetBounds(graphics);
+
+            return (int)(rect.Right + 1.0f);
+        }
+
+        float MeasureString(string text, Font font)
+        {
+            Graphics g = Graphics.FromImage(new Bitmap(1, 1));
+            return g.MeasureString(text + "|", font, this.Width, StringFormat.GenericTypographic).Width - g.MeasureString("|", this.Font, PointF.Empty, StringFormat.GenericTypographic).Width;
+        }
+
+        static public int MeasureDisplayStringWidth1(Graphics graphics, string text,
+                                            Font font)
+        {
+            const int width = 32;
+
+            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(width, 1, graphics);
+            System.Drawing.SizeF size = graphics.MeasureString(text, font);
+            System.Drawing.Graphics anagra = System.Drawing.Graphics.FromImage(bitmap);
+
+            int measured_width = (int)size.Width;
+
+            if (anagra != null)
+            {
+                anagra.Clear(Color.White);
+                anagra.DrawString(text + "|", font, Brushes.Black,
+                                   width - measured_width, -font.Height / 2);
+
+                for (int i = width - 1; i >= 0; i--)
+                {
+                    measured_width--;
+                    if (bitmap.GetPixel(i, 0).R != 255)    // found a non-white pixel ?
+                        break;
+                }
             }
 
-            tblpPrimary.Visible = false;
+            return measured_width;
         }
     }
 }
