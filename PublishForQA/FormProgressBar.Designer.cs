c43388a51b301137f0e80f18bf6872fe18513222
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-02-20 11:06:39 +0200

    Finished the progress bar form.

--- PublishForQA/FormProgressBar.Designer.cs
+++ PublishForQA/FormProgressBar.Designer.cs
@@ -28,11 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProgressBar));
+            this.lblCurrentOperation = new System.Windows.Forms.Label();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.lblCurrentPath = new System.Windows.Forms.Label();
+            this.pbMain = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tlpMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCurrentOperation
+            // 
+            this.lblCurrentOperation.AutoSize = true;
+            this.lblCurrentOperation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCurrentOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCurrentOperation.Location = new System.Drawing.Point(10, 10);
+            this.lblCurrentOperation.Margin = new System.Windows.Forms.Padding(5);
+            this.lblCurrentOperation.Name = "lblCurrentOperation";
+            this.lblCurrentOperation.Size = new System.Drawing.Size(524, 20);
+            this.lblCurrentOperation.TabIndex = 0;
+            this.lblCurrentOperation.Text = "Error in operation label";
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 1;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.Controls.Add(this.lblCurrentOperation, 0, 0);
+            this.tlpMain.Controls.Add(this.lblCurrentPath, 0, 1);
+            this.tlpMain.Controls.Add(this.pbMain, 0, 2);
+            this.tlpMain.Controls.Add(this.btnCancel, 0, 3);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.Padding = new System.Windows.Forms.Padding(5);
+            this.tlpMain.RowCount = 4;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpMain.Size = new System.Drawing.Size(544, 150);
+            this.tlpMain.TabIndex = 1;
+            // 
+            // lblCurrentPath
+            // 
+            this.lblCurrentPath.AutoSize = true;
+            this.lblCurrentPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCurrentPath.Location = new System.Drawing.Point(10, 40);
+            this.lblCurrentPath.Margin = new System.Windows.Forms.Padding(5);
+            this.lblCurrentPath.Name = "lblCurrentPath";
+            this.lblCurrentPath.Size = new System.Drawing.Size(524, 20);
+            this.lblCurrentPath.TabIndex = 1;
+            this.lblCurrentPath.Text = "Error in path label";
+            // 
+            // pbMain
+            // 
+            this.pbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbMain.Location = new System.Drawing.Point(8, 68);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(528, 34);
+            this.pbMain.TabIndex = 2;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(234, 113);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // FormProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormProgressBar";
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(544, 150);
+            this.ControlBox = false;
+            this.Controls.Add(this.tlpMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormProgressBar";
+            this.Text = "Progress";
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCurrentOperation;
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Label lblCurrentPath;
+        private System.Windows.Forms.ProgressBar pbMain;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
