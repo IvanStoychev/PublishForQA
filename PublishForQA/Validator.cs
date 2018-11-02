fd1cee2d75bf3fdefa27174591c12de59836f2b8
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-11-03 00:00:34 +0200

    Created "Validator" class and skeleton for "PathLength" method for checking if any paths are longer than the allowed maximum.

---
+++ PublishForQA/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublishForQA
+{
+    static class Validator
+    {
+        static void Validate()
+        {
+
+        }
+
+        static void PathLength()
+        {
+
+        }
+    }
+}
