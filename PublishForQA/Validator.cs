7d079639ba2d28d607af1afda07067d83836ed6c
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-11-03 18:48:31 +0200

    Renamed "PathLength" to "GetLongestPath" and worked on its implementation.

--- PublishForQA/Validator.cs
+++ PublishForQA/Validator.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PublishForQA
 {
     static class Validator
     {
-        static void Validate()
+        static void Validate(TextBox[] textBoxes)
         {
 
         }
 
-        static void PathLength()
+        /// <summary>
+        /// Finds the longest system entry path from
+        /// </summary>
+        /// <param name="directories"></param>
+        /// <returns></returns>
+        static string GetLongestPath(DirectoryInfo[] directories)
         {
+            string longestPath = string.Empty;
 
+            foreach (var dir in directories)
+            {
+                string path = dir.EnumerateFileSystemInfos().OrderByDescending(d => d.FullName).FirstOrDefault().FullName;
+                if (path.Length > longestPath.Length) longestPath = path;
+            }
+
+            return longestPath;
         }
     }
 }
