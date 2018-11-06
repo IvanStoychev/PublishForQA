ffb03410bdb18bedd7bb72b83af77cb77ed52d60
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-11-06 13:36:24 +0200

    Added error icon and changed ControlValidationCheck's design skeleton.

--- PublishForQA/ControlValidationCheck.Designer.cs
+++ PublishForQA/ControlValidationCheck.Designer.cs
@@ -29,65 +29,107 @@
         private void InitializeComponent()
         {
             this.lblValidationName = new System.Windows.Forms.Label();
-            this.splitContainer = new System.Windows.Forms.SplitContainer();
-            this.splitterTop = new System.Windows.Forms.Splitter();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
-            this.splitContainer.Panel1.SuspendLayout();
-            this.splitContainer.SuspendLayout();
+            this.pbError = new System.Windows.Forms.PictureBox();
+            this.pbExpandCollapse = new System.Windows.Forms.PictureBox();
+            this.tlpTopHalf = new System.Windows.Forms.TableLayoutPanel();
+            this.splitMain = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.pbError)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExpandCollapse)).BeginInit();
+            this.tlpTopHalf.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitMain)).BeginInit();
+            this.splitMain.Panel1.SuspendLayout();
+            this.splitMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblValidationName
             // 
             this.lblValidationName.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblValidationName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblValidationName.Location = new System.Drawing.Point(77, 0);
+            this.lblValidationName.Location = new System.Drawing.Point(33, 0);
             this.lblValidationName.Name = "lblValidationName";
-            this.lblValidationName.Size = new System.Drawing.Size(434, 74);
-            this.lblValidationName.TabIndex = 1;
+            this.lblValidationName.Size = new System.Drawing.Size(354, 30);
+            this.lblValidationName.TabIndex = 0;
             this.lblValidationName.Text = "lblValidationName";
             this.lblValidationName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // splitContainer
+            // pbError
             // 
-            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer.Name = "splitContainer";
-            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.pbError.Image = global::PublishForQA.Properties.Resources.error_icon_4;
+            this.pbError.Location = new System.Drawing.Point(3, 3);
+            this.pbError.Name = "pbError";
+            this.pbError.Size = new System.Drawing.Size(24, 24);
+            this.pbError.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbError.TabIndex = 2;
+            this.pbError.TabStop = false;
             // 
-            // splitContainer.Panel1
+            // pbExpandCollapse
             // 
-            this.splitContainer.Panel1.Controls.Add(this.lblValidationName);
-            this.splitContainer.Panel1.Controls.Add(this.splitterTop);
-            this.splitContainer.Size = new System.Drawing.Size(511, 303);
-            this.splitContainer.SplitterDistance = 74;
-            this.splitContainer.TabIndex = 2;
+            this.pbExpandCollapse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbExpandCollapse.Image = global::PublishForQA.Properties.Resources.icon_help_circled_128;
+            this.pbExpandCollapse.Location = new System.Drawing.Point(393, 3);
+            this.pbExpandCollapse.Name = "pbExpandCollapse";
+            this.pbExpandCollapse.Size = new System.Drawing.Size(25, 24);
+            this.pbExpandCollapse.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbExpandCollapse.TabIndex = 1;
+            this.pbExpandCollapse.TabStop = false;
             // 
-            // splitterTop
+            // tlpTopHalf
             // 
-            this.splitterTop.Location = new System.Drawing.Point(0, 0);
-            this.splitterTop.Name = "splitterTop";
-            this.splitterTop.Size = new System.Drawing.Size(77, 74);
-            this.splitterTop.TabIndex = 0;
-            this.splitterTop.TabStop = false;
+            this.tlpTopHalf.ColumnCount = 3;
+            this.tlpTopHalf.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tlpTopHalf.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 360F));
+            this.tlpTopHalf.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpTopHalf.Controls.Add(this.pbExpandCollapse, 2, 0);
+            this.tlpTopHalf.Controls.Add(this.lblValidationName, 1, 0);
+            this.tlpTopHalf.Controls.Add(this.pbError, 0, 0);
+            this.tlpTopHalf.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpTopHalf.Location = new System.Drawing.Point(0, 0);
+            this.tlpTopHalf.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpTopHalf.Name = "tlpTopHalf";
+            this.tlpTopHalf.RowCount = 1;
+            this.tlpTopHalf.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tlpTopHalf.Size = new System.Drawing.Size(416, 30);
+            this.tlpTopHalf.TabIndex = 0;
+            // 
+            // splitMain
+            // 
+            this.splitMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitMain.Location = new System.Drawing.Point(0, 0);
+            this.splitMain.Name = "splitMain";
+            this.splitMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitMain.Panel1
+            // 
+            this.splitMain.Panel1.Controls.Add(this.tlpTopHalf);
+            this.splitMain.Size = new System.Drawing.Size(416, 210);
+            this.splitMain.SplitterDistance = 30;
+            this.splitMain.SplitterWidth = 1;
+            this.splitMain.TabIndex = 0;
+            this.splitMain.TabStop = false;
             // 
             // ControlValidationCheck
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.splitMain);
             this.Name = "ControlValidationCheck";
-            this.Size = new System.Drawing.Size(511, 303);
-            this.splitContainer.Panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
-            this.splitContainer.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(416, 210);
+            ((System.ComponentModel.ISupportInitialize)(this.pbError)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExpandCollapse)).EndInit();
+            this.tlpTopHalf.ResumeLayout(false);
+            this.splitMain.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitMain)).EndInit();
+            this.splitMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Label lblValidationName;
-        private System.Windows.Forms.SplitContainer splitContainer;
-        private System.Windows.Forms.Splitter splitterTop;
+        private System.Windows.Forms.PictureBox pbError;
+        private System.Windows.Forms.PictureBox pbExpandCollapse;
+        private System.Windows.Forms.TableLayoutPanel tlpTopHalf;
+        private System.Windows.Forms.SplitContainer splitMain;
     }
 }
--- PublishForQA/Properties/Resources.Designer.cs
+++ PublishForQA/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace PublishForQA.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap error_icon_4 {
+            get {
+                object obj = ResourceManager.GetObject("error_icon_4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon_help_circled_128 {
             get {
                 object obj = ResourceManager.GetObject("icon-help-circled-128", resourceCulture);
