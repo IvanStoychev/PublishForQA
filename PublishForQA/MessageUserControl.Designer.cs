c8480cd9e8c356eb25dc8b4ae2141645d1fbbd6d
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-12-07 17:01:53 +0200

    Fleshed out the body and design of the MessageUserControl.

--- PublishForQA/MessageUserControl.Designer.cs
+++ PublishForQA/MessageUserControl.Designer.cs
@@ -31,6 +31,7 @@
             this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
             this.pbIcon = new System.Windows.Forms.PictureBox();
             this.btnFix = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
             this.tlpMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
             this.SuspendLayout();
@@ -40,9 +41,10 @@
             this.tlpMain.ColumnCount = 3;
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 53F));
             this.tlpMain.Controls.Add(this.pbIcon, 0, 0);
             this.tlpMain.Controls.Add(this.btnFix, 2, 0);
+            this.tlpMain.Controls.Add(this.lblMessage, 1, 0);
             this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpMain.Location = new System.Drawing.Point(0, 0);
             this.tlpMain.Name = "tlpMain";
@@ -65,13 +67,24 @@
             // btnFix
             // 
             this.btnFix.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnFix.Location = new System.Drawing.Point(373, 17);
+            this.btnFix.Location = new System.Drawing.Point(396, 17);
             this.btnFix.Name = "btnFix";
             this.btnFix.Size = new System.Drawing.Size(40, 23);
             this.btnFix.TabIndex = 1;
             this.btnFix.Text = "Fix";
             this.btnFix.UseVisualStyleBackColor = true;
             this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMessage.Location = new System.Drawing.Point(43, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(344, 57);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Message";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MessageUserControl
             // 
@@ -81,6 +94,7 @@
             this.Name = "MessageUserControl";
             this.Size = new System.Drawing.Size(443, 57);
             this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
             this.ResumeLayout(false);
 
@@ -91,5 +105,6 @@
         private System.Windows.Forms.TableLayoutPanel tlpMain;
         private System.Windows.Forms.PictureBox pbIcon;
         private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.Label lblMessage;
     }
 }
--- PublishForQA/MessageUserControl.cs
+++ PublishForQA/MessageUserControl.cs
@@ -1,18 +1,46 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using static PublishForQA.Enums;
 
 namespace PublishForQA
 {
     public partial class MessageUserControl : UserControl
     {
-        public MessageUserControl(string message, MessageUserControlIcons icon, MessageUserControlButtons buttons)
+        Func<bool> fixMethod = null;
+
+        public MessageUserControl(string message, MessageUserControlIcons icon, MessageUserControlButtons buttons, Func<bool> func = null)
         {
             InitializeComponent();
+            lblMessage.Text = message;
+            fixMethod = func;
+
+            switch (icon)
+            {
+                case MessageUserControlIcons.Warning:
+                    pbIcon.Image = Properties.Resources.Warning;
+                    break;
+                case MessageUserControlIcons.Error:
+                    pbIcon.Image = Properties.Resources.Error;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (buttons)
+            {
+                case MessageUserControlButtons.None:
+                    btnFix.Visible = false;
+                    break;
+                case MessageUserControlButtons.Fix:
+                    break;
+                default:
+                    break;
+            }
         }
 
-        private void btnFix_Click(object sender, System.EventArgs e)
+        private void btnFix_Click(object sender, EventArgs e)
         {
-
+            btnFix.Enabled = !fixMethod();
         }
     }
 }
