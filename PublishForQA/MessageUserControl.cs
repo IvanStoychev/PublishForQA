7f368f6cda8b7dac1076f149758ea176cd793fe1
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-12-07 17:48:49 +0200

    Added summaries.

--- PublishForQA/MessageUserControl.cs
+++ PublishForQA/MessageUserControl.cs
@@ -4,10 +4,24 @@ using static PublishForQA.Enums;
 
 namespace PublishForQA
 {
+    /// <summary>
+    /// Displays an error or warning message, concerning failed validations
+    /// for the copy operation.
+    /// </summary>
     public partial class MessageUserControl : UserControl
     {
+        /// <summary>
+        /// The method the "Fix" button will call when clicked.
+        /// </summary>
         Func<bool> fixMethod = null;
 
+        /// <summary>
+        /// Creates a new instance of the MessageUserControl class, displaying the given message, icon and buttons.
+        /// </summary>
+        /// <param name="message">The message to display in the label control.</param>
+        /// <param name="icon">The type of icon to display.</param>
+        /// <param name="buttons">The type of buttons to display.</param>
+        /// <param name="func">The method to be called on button click.</param>
         public MessageUserControl(string message, MessageUserControlIcons icon, MessageUserControlButtons buttons, Func<bool> func = null)
         {
             InitializeComponent();
@@ -40,6 +54,8 @@ namespace PublishForQA
 
         private void btnFix_Click(object sender, EventArgs e)
         {
+            // If the fixMethod succeeds, and thus returns "true",
+            // disable the button so the operation can't be doubled.
             btnFix.Enabled = !fixMethod();
         }
     }
